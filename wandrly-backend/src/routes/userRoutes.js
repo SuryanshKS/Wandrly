9ae377641c5eec66db163c0478effb21a30c4728
SuryanshKS <suryanshsks2005@gmail.com>
2026-05-19 19:20:07 +0530

    GET/api/users/me

--- wandrly-backend/src/routes/userRoutes.js
+++ wandrly-backend/src/routes/userRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import {registerUser,authUser,getUserProfile} from '../controllers/userController.js';
+import { protect } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
