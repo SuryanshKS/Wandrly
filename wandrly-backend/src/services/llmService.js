b7ea7381ed2349b04a708df274a1ed73b55a6977
SuryanshKS <suryanshsks2005@gmail.com>
2026-05-20 19:38:00 +0530

    2.0 flash

--- wandrly-backend/src/services/llmService.js
+++ wandrly-backend/src/services/llmService.js
@@ -13,7 +13,7 @@ export const generativeStructuredAIResponse = async (systemPrompt, userContext) 
     try {
         //we use flash as its fast and handles JSON perfectly
         const model = genAI.getGenerativeModel({
-            model: "gemini-1.5-flash",
+            model: "gemini-2.0-flash",
             generationConfig: {
                 responseMimeType: "application/json",
             }
