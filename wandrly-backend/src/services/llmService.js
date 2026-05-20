f1d12c71da4aa1565bea907d8c92e1c00406390e
SuryanshKS <suryanshsks2005@gmail.com>
2026-05-20 19:43:05 +0530

    changed .env GEMINI_API_KEY to bypass RPD on previous key

--- wandrly-backend/src/services/llmService.js
+++ wandrly-backend/src/services/llmService.js
@@ -13,7 +13,7 @@ export const generativeStructuredAIResponse = async (systemPrompt, userContext) 
     try {
         //we use flash as its fast and handles JSON perfectly
         const model = genAI.getGenerativeModel({
-            model: "gemini-2.0-flash",
+            model: "gemini-2.5-flash",
             generationConfig: {
                 responseMimeType: "application/json",
             }
