babda518f8ed4a531d0ef2466c43d4b5ec1017f0
SuryanshKS <suryanshsks2005@gmail.com>
2026-05-21 17:18:26 +0530

    added missing prisma import in media controller

--- wandrly-backend/src/controllers/mediaController.js
+++ wandrly-backend/src/controllers/mediaController.js
@@ -1,3 +1,4 @@
+import prisma from "../config/prisma.js";
 import { getPacingAnalytics } from "../services/analyticsService.js";
 import { compileChronologicalTravelogue, processMediaUpload, removeMediaRecord } from "../services/mediaService.js";
 import asyncHandler from "../utils/asyncHandler.js";
