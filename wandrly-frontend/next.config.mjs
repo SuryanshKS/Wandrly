2b0bdfd60727c75dba1a3766a1d68b7b030ccc9b
SuryanshKS <suryanshsks2005@gmail.com>
2026-05-23 14:26:02 +0530

    config changes

--- wandrly-frontend/next.config.mjs
+++ wandrly-frontend/next.config.mjs
@@ -1,6 +1,6 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
+const nextConfig = {
     trailingSlash: true,
   /* config options here */
 };
